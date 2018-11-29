c7ce688a89e6dcb6147dd8eec9ee3da388e2e955
alexeytas <alexdali@gmail.com>
2018-11-29 12:04:34 +0600

    1c: added buttons

--- src/index.js
+++ src/index.js
@@ -1,12 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Avatar() {
+	return (
+		<img
+		// {src="src/pictures/avatar.jpg"}
+		src="https://image.freepik.com/free-vector/moustache-man-cartoon-vector_17-1018103121.jpg"
+		className="avatar"
+		alt="avatar" />
+	);
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Time = () => (
+	<span className = 'time'>3h ago</span>
+);
+
+const ReplyButton = () => (
+	<i className = 'fa fa-reply reply-button' />
+);
+
+const RetweetButton = () => (
+	<i className = 'fa fa-retweet retweet-button' />
+);
+
+const LikeButton = () => (
+	<i className = 'fa fa-heart like-button' />
+);
+
+const MoreOptionsButton = () => (
+	<i className = 'fa fa-ellipsis-h more-options-button' />
+);
+
+function Message() {
+	return (
+		<div className = 'message'>
+			This is less than 140 characters.
+		</div>
+	);
+}
+
+function NameWithHandle() {
+	return (
+		<span className = 'name-with-handle'>
+			<span className = 'name'>Your Name</span>
+			<span className = 'handle'>@yourhandle</span>
+		</span>
+	);
+}
+
+function Tweet() {
+	return (
+		<div className="tweet">
+			<Avatar />
+			<div className = 'content'>
+				<NameWithHandle /><Time />
+				<Message />
+				<div className = 'buttons'>
+					<ReplyButton />
+					<RetweetButton />
+					<LikeButton />
+					<MoreOptionsButton />
+				</div>
+			</div>
+		</div>
+	);
+}
+
+ReactDOM.render(<Tweet/>,
+document.querySelector('#root'));
